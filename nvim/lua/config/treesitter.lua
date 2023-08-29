37c8df81accd3166008cc9147387fca95b959251
roycy.tang <roycy.tang@okg.com>
2023-08-29 19:17:36 +0800

    treesit; mappings

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,44 +1,49 @@
 require('nvim-treesitter.configs').setup {
-  auto_install = true,
-  highlight = { enable = true },
-  indent = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<cr>',
-      node_incremental = '<tab>',
-      scope_incremental = '<cr>',
-      scope_decremental = '<s-cr>',
-      node_decremental = '<s-tab>',
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<cr>',
+            node_incremental = '<tab>',
+            scope_incremental = '<cr>',
+            scope_decremental = '<s-cr>',
+            node_decremental = '<s-tab>',
+        },
     },
-  },
-  refactor = {
-    smart_rename = { enable = true, keymaps = { smart_rename = 'grr' } },
-    highlight_definitions = { enable = true },
-  },
-  textsubjects = {
-    enable = true,
-    lookahead = true,
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
+    refactor = {
+        smart_rename = { enable = true, keymaps = { smart_rename = 'grr' } },
+        highlight_definitions = { enable = true },
     },
-    keymaps = {
-      ['.'] = 'textsubjects-smart',
-      [';'] = 'textsubjects-container-outer',
-      ['i;'] = 'textsubjects-container-inner',
+    textsubjects = {
+        enable = true,
+        lookahead = true,
+        select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ['aa'] = '@parameter.outer',
+                ['ia'] = '@parameter.inner',
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+            },
+            selection_modes = {
+                ['@parameter.outer'] = 'v', -- charwise
+                ['@function.outer'] = 'V', -- linewise
+                ['@class.outer'] = '<c-v>', -- blockwise
+            },
+        },
+        keymaps = {
+            ['.'] = 'textsubjects-smart',
+            [';'] = 'textsubjects-container-outer',
+            ['i;'] = 'textsubjects-container-inner',
+        },
     },
-  },
-  endwise = { enable = true },
-  matchup = { enable = true, include_match_words = true, enable_quotes = true },
-  autotag = { enable = true },
+    endwise = { enable = true },
+    matchup = { enable = true, include_match_words = true, enable_quotes = true },
+    autotag = { enable = true },
 }
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -85,6 +85,9 @@ return {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         event = 'BufReadPost',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects'
+        },
         config = function()
             require 'config.treesitter'
         end,
