4e60937738690818e9cdd36686e5646f5fb303a0
roycy.tang <roycy.tang@okg.com>
2024-04-12 10:09:58 +0800

    treesitter disable for ft=log

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -3,6 +3,7 @@ require('nvim-treesitter.configs').setup {
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
+        disable = { "log" },
     },
     indent = { enable = true },
     refactor = {
